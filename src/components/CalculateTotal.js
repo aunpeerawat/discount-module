bec9ba290522cd7a3a51b81daeb08ad69ac8b972
Peerawat <peerawat.okn@gmail.com>
2024-02-11 13:46:10 +0700

    add coupon discount function

--- src/components/CalculateTotal.js
+++ src/components/CalculateTotal.js
@@ -2,8 +2,19 @@ import React, {useState} from "react";
 
 function CalculateTotal(props){
     const {Clothing,Accessories,Electronics,All} = props.cartTotal
-    console.log(Clothing,Accessories,Electronics,All);
-    return(<p>It's calculate total!</p>)
-}
+    const discounts=props.discount;
+    let total = All;
+    if (discounts.some(discount=>discount.type==="coupon")){
+        const index = discounts.findIndex(discount=>discount.type==="coupon");
+        if (discounts[index].campaigns==="Fixed"){
+            total = total - discounts[index].amount;
+        }
+        else if (discounts[index].campaigns==="PercentDis"){
+            total = total - ((discounts[index].amount)*All/100);
+        }
+        console.log(total);
+    }
+    return(<p>It's calculate total!</p>)}
+
 
 export default CalculateTotal;
